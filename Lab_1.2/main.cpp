1305e07c31f3618589c29828bd6be7d50fa4dd98
neksus-hub <66494964+neksus-hub@users.noreply.github.com>
2022-09-18 16:28:32 +0300

    Add files via upload

---
+++ Lab_1.2/main.cpp
@@ -0,0 +1,26 @@
+#include "slave.h"
+#include <string>
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+int a; int b = 0; string s;
+cout << "\t\t\t\t-----MENU-----"<< endl;
+cout << "\t\tПРОГРАММА ДЛЯ ШИФРОВАНИЯ МЕТОДОМ МАРШРУТНОЙ КАРТЫ" << endl;
+cout << "Введите ключ: ";
+cin >> a; cout << endl;
+modRoutingCipher c{a};
+cout << "1) Зашифровать сообщение" << endl; cout << "2) Дешифровать сообщение" << endl; 
+cin >> b;
+cout << "Введите сообщение: " << endl;
+cin >> s;
+if (b == 1) {
+string s1 = c.encrypt(s);
+cout << "Зашифрованное сообщение: " << s1 << endl;
+}
+if (b == 2) {
+string s2 = c.decrypt(s);
+cout << "Дешифрованное сообщение: " << s2 << endl;
+}
+return 0;
+}
