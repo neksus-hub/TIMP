8f95dfe67af4d27caccca88395709fafcca5c614
neksus-hub <66494964+neksus-hub@users.noreply.github.com>
2022-10-02 21:57:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
+#include <codecvt>
+#include <typeinfo>
+using namespace std;
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<L"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        wcerr<<"Error: "<<e.what()<<endl;
+    }
+    
+}
+
+int main()
+{
+    check(L"city",L"roads");
+    check(L"roads",L"");
+    check(L"sashka",L"city058");
+    check(L"r o a d s",L"CITY");
+    check(L"058",L"CITY");
+}
